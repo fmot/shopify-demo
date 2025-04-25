ccbdb7764713a32c9271107c5b286812d20aa7b2
Fumiya Otake <62979220+fmot@users.noreply.github.com>
2025-04-25 15:53:14 +0900

    Update BulkPriceUpdateContainer.jsx

--- web/frontend/components/BulkPriceUpdateContainer.jsx
+++ web/frontend/components/BulkPriceUpdateContainer.jsx
@@ -105,7 +105,7 @@ export function BulkPriceUpdateContainer() {
         return {
           productId: product.id,
           variantId,
-          price: priceUpdates[variantId] ?? variant.price,
+          price: priceUpdates[variantId] || variant.price,
         };
       });
 
